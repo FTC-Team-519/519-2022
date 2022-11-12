5f0804b59aa5aa47f7f41ac2f409efcb4955a4ac
ftc519 <ftc519@gmail.com>
2022-11-12 13:07:41 -0500

    Should not work

--- src/main/java/org/firstinspires/ftc/teamcode/TensorFlowDection.java
+++ src/main/java/org/firstinspires/ftc/teamcode/TensorFlowDection.java
@@ -52,7 +52,6 @@ import java.util.List;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection Webcam", group = "Concept")
-
 public class TensorFlowDection extends LinearOpMode {
 
     /*
